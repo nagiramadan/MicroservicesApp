e70b32a695dc21db17c2c5bb16ad2fda89516a0d
Nagi Ramadan <nagiramadan@users.noreply.github.com>
2021-01-05 01:35:44 +0200

    add docker to catalog api

--- src/Catalog/Catalog.Api/Controllers/CatalogController.cs
+++ src/Catalog/Catalog.Api/Controllers/CatalogController.cs
@@ -1,12 +1,89 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Catalog.Api.Entities;
+using Catalog.Api.Repositories.Interface;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Threading.Tasks;
 
 namespace Catalog.Api.Controllers
 {
+    [ApiController]
+    [Route("api/v1/[controller]")]
     public class CatalogController : ControllerBase
     {
+        private readonly IProductRepository _repository;
+        private readonly ILogger<CatalogController> _logger;
+
+        public CatalogController(IProductRepository repository, ILogger<CatalogController> logger)
+        {
+            _repository = repository;
+            _logger = logger;
+        }
+
+        [HttpGet]
+        [ProducesResponseType(typeof(List<Product>), (int)HttpStatusCode.OK)]
+        public async Task<IActionResult> GetProducts()
+        {
+            var result = await _repository.GetProductsAsync();
+            return Ok(result);
+        }
+
+        [HttpGet("{id}", Name = "GetProduct")]
+        [ProducesResponseType((int)HttpStatusCode.NotFound)]
+        [ProducesResponseType(typeof(Product), (int)HttpStatusCode.OK)]
+        public async Task<IActionResult> GetProduct(string id)
+        {
+            var result = await _repository.GetProductAsync(id);
+            if (result == null)
+            {
+                _logger.LogError($"Product with id: {id} not found");
+                return NotFound();
+            }
+            return Ok(result);
+        }
+
+
+        [HttpGet("[action]/{category}")]
+        [ProducesResponseType((int)HttpStatusCode.NotFound)]
+        [ProducesResponseType(typeof(List<Product>), (int)HttpStatusCode.OK)]
+        public async Task<IActionResult> GetProductsByCategory(string category)
+        {
+            var result = await _repository.GetProductByCategoryAsync(category);
+            return Ok(result);
+        }
+
+
+        [HttpPost]
+        public async Task<IActionResult> CreateProduct(Product product)
+        {
+            await _repository.CreateAsync(product);
+            return CreatedAtRoute("GetProduct", new { id = product.Id }, product);
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> UpdateProduct(Product product)
+        {
+            var result = await _repository.UpdateAsync(product);
+            return Ok(result);
+        }
+
+        [HttpGet("[action]/{name}")]
+        [ProducesResponseType((int)HttpStatusCode.NotFound)]
+        [ProducesResponseType(typeof(List<Product>), (int)HttpStatusCode.OK)]
+        public async Task<IActionResult> GetProductsByName(string name)
+        {
+            var result = await _repository.GetProductByNameAsync(name);
+            return Ok(result);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteProduct(string id)
+        {
+            var result = await _repository.DeleteAsync(id);
+            return Ok(result);
+        }
     }
 }
--- src/Catalog/Catalog.Api/Startup.cs
+++ src/Catalog/Catalog.Api/Startup.cs
@@ -11,6 +11,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Microsoft.OpenApi.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -35,6 +36,9 @@ namespace Catalog.Api
             services.AddSingleton<ICatalogDatabaseSettings>(sp => sp.GetRequiredService<IOptions<CatalogDatabaseSettings>>().Value);
             services.AddTransient<ICatalogContext, CatalogContext>();
             services.AddTransient<IProductRepository, ProductRepository>();
+            services.AddSwaggerGen(c => {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Catelog Api", Version = "v1" });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -52,6 +56,12 @@ namespace Catalog.Api
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
+            });
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Catelog API V1");
             });
         }
     }
